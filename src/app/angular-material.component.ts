22325246b705b10f01a813a837b7f0493d1c8fdf
alejacadavid <mariaa.cadavid@udea.edu.co>
2017-09-02 22:12:24 -0500

    Actividad 3 Opcional - Material Design

---
+++ src/app/angular-material.component.ts
@@ -0,0 +1,25 @@
+
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+
+export class User {
+  constructor(public name: string) { }
+}
+
+
+@Component({
+  selector: 'angular-material',
+  templateUrl: './angular-material.component.html',
+  styleUrls: [ './angular-material.component.css']
+})
+export class AngularMaterialComponent {
+
+ 
+
+}
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,49 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+ 
 import { AppRoutingModule } from './app-routing.module';
+ 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MdChipsModule} from '@angular/material';
+import {MdCardModule} from '@angular/material';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
+ 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
 import { HeroSearchComponent }  from './hero-search.component';
-
+import { AngularMaterialComponent }  from './angular-material.component';
+ 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdChipsModule,
+    MdCardModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    AngularMaterialComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
